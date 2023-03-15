846ddf1b6e14801c3eec421e76a6ff9f48dc1869
abukawsar45 <kawsar1146236@gmail.com>
2023-03-15 17:37:20 +0600

    random part fix

---
+++ taglineGenarator.js
@@ -0,0 +1,83 @@
+const tagLines = [
+  "Abu Kaw Sar",
+  "Kawsar",
+  "RaSeL",
+  "MoSToFa",
+  "NuSRaT",
+  "FaTeMa",
+  "KeYa",
+  "34",
+  "349",
+  "3421",
+  "343",
+  "34454",
+  "3445",
+  "Kamrunnaher",
+  "Kamrunnaher Keya",
+  "Okey",
+];
+
+function generateTag(arr, interval){
+  setInterval (() =>{
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const randomTagLine = tagLines[randomIndex];
+    console.log(randomTagLine);
+    document.getElementById('tagline').innerHTML = randomTagLine
+  },interval)
+}
+generateTag(tagLines, 3000)
+
+
+document
+  .getElementById("random-room-btn")
+  .addEventListener("click", async function () {
+    console.log("hello world");
+    const data = await fetch(`ROOMS.json`);
+    const result = await data.json();
+    const randomIndex = Math.floor(Math.random() * 10);
+    const modalBody = document.getElementById("random-room-info-modal-body");
+    const { name, summary, property_type, images, review_scores } = result[9];
+
+
+
+    const reviews = document.createElement("ol");
+    if (review_scores.scores) {
+      reviews.setAttribute('class', "list-group")
+
+      object.keys(review_scores.scores).forEach((key) => {
+        reviews.innerHTML += `
+        <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
+        ${key} : 
+        <span class="badge bg-primary rounded-pill">${review_scores.scores[key]}</span> 
+        </li>
+        `;
+      });
+    } else {
+      reviews.innerHTML = "No reviews found";
+    }
+
+
+    modalBody.innerHTML = `
+    <div class="col">
+    <div class="card h-100">
+      <img src=${images.picture_url} class="card-img-top " alt="..." style=" height: 300px;
+      object-fit: fill;">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${property_type}</p>
+        <p class="card-text">${summary}</p>
+        <div id='review-score'>
+      Review Scores :
+      ${review_scores.scores.review_scores_accuracy}
+    </div>
+      </div>
+      <button class="btn btn-info btn-lg"  role="button"
+                   >Show details</button>
+    </div>
+   </div>
+    `;
+
+  });
+
+
+
